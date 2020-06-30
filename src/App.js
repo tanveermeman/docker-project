aabc50d94e3cd1fcb972e4764241450f1358e16c
techkman <tanveermeman786@gmail.com>
2020-06-30 19:35:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+		This is updated in realti me and does not need to rebuild the project.
         </p>
         <a
           className="App-link"
